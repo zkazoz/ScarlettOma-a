c12cdd115ae1e863adfd3f39ae121cdb0c00ae89
SCARLETT OMAÑA <scarlettomaa@gmail.com>
2017-10-17 12:03:13 -0500

    clase 10oct

--- Rectangle/Rectangle/InputFile.cpp
+++ Rectangle/Rectangle/InputFile.cpp
@@ -6,22 +6,29 @@
 
 bool InputFile::Read(std::string filename)
 {
-	std::fstream fs;
-	fs.open("Prueba.txt");
-	
-		if (fs.is_open()) {
-		std::stringstream ss;
-		ss << fs.rdbuf();
-		_contents = ss.str();
-		return true;
-
-	}
-		else {
-			return false;
+	if (filename.empty())
+		 {
+		std::cout << "No filename provided" << std::endl;
+		return false;
 		}
+	
+		std::fstream inputFile(filename, std::fstream::in);
+	
+		if (!inputFile.is_open())
+		 {
+		std::cout << "Could not open file " << filename << std::endl;
+		return false;
+		}
+	
+		std::stringstream ss;
+	ss << inputFile.rdbuf();
+	_contents = ss.str();
+	
+		return true;
+	
 }
 
 std::string InputFile::GetContents()
-{
-	return std::string(_contents);
-}
+ {
+	return _contents;
+	}
